c037dfac9a824d883db91d58b0ba16546d3ca0d9
Sunny kumar <sunnyku9036@gmail.com>
2026-07-15 00:03:10 +0530

    completed  project 2 calculating BMI weight  of  an javascript

--- basic 07 projects/project2/script.js
+++ basic 07 projects/project2/script.js
@@ -1,17 +1,29 @@
-const form=document.querySelector('form')
+const form = document.querySelector('form');
 
-  //this usecase wil give empty
-  // const height=parseInt(document.querySelector('#height').value)
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
 
-form.addEventListener('submit',function(e){
-  e.preventDefault()
+  const height = parseFloat(document.querySelector('#height').value);
+  const weight = parseFloat(document.querySelector('#weight').value);
+  const results = document.querySelector('#results');
 
-  const height=parseInt(document.querySelector('#height').value)
-  const weight=parseInt(document.querySelector('#height').value)
-  const results=document.querySelector('#results')
-
-  if(height ===''|| height<0|| isNaN(height) ){
-    results.innerHTML ="please give a valid height";
+  if (isNaN(height) || height <= 0) {
+    results.innerHTML = `Please give a valid height`;
+    return;
   }
-  results.inn
-})
+
+  if (isNaN(weight) || weight <= 0) {
+    results.innerHTML = `Please give a valid weight`;
+    return;
+  }
+
+  const bmi = weight / ((height * height) / 10000);
+
+  if (bmi < 18.6) {
+    results.innerHTML = `BMI: ${bmi.toFixed(2)} - Under Weight`;
+  } else if (bmi >= 18.6 && bmi <= 24.9) {
+    results.innerHTML = `BMI: ${bmi.toFixed(2)} - Normal Weight`;
+  } else {
+    results.innerHTML = `BMI: ${bmi.toFixed(2)} - Over Weight`;
+  }
+});
