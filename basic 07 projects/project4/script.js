37a88cf22e4b0c01bd515541e3d365e182c0d521
Sunny kumar <sunnyku9036@gmail.com>
2026-07-17 18:35:42 +0530

    created a project of asycn and bulided random number generator  in js

---
+++ basic 07 projects/project4/script.js
@@ -0,0 +1,31 @@
+const randomNumber=parseInt(Math.random()*100+1)
+
+const submit = document.querySelector('#subt');
+const userInput = document.querySelector('#guessField');
+const guessSlot = document.querySelector('.guesses');
+const remaining = document.querySelector('.lastResult');
+const lowOrHeigh = document.querySelector('.lowOrHi');
+const startover = document.querySelector('.resultParas');
+
+
+const p = document.createElement('p')
+
+let prevGuess = []
+let numGuess = 1
+
+let playGame = true;
+
+function validateGuess(guess){
+
+}
+
+function checkGuess(guess){
+
+}
+
+
+FocusEvent
+
+function displayMessage(messgae){
+
+}
